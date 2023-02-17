76e657f75150a9c3a46cd93ff24066d802a01d28
John Gilbert <johngilb@gmail.com>
2023-02-16 23:38:10 -0800

    Initial ideas for routing the arm in a simple way

--- src/main/java/competition/subsystems/arm/commands/SetArmsToPositionCommand.java
+++ src/main/java/competition/subsystems/arm/commands/SetArmsToPositionCommand.java
@@ -2,14 +2,13 @@ package competition.subsystems.arm.commands;
 
 import competition.subsystems.arm.UnifiedArmSubsystem;
 import xbot.common.command.BaseSetpointCommand;
-import xbot.common.math.XYPair;
 import xbot.common.properties.PropertyFactory;
 
 import javax.inject.Inject;
-import xbot.common.properties.DoubleProperty;
 
 public class SetArmsToPositionCommand extends BaseSetpointCommand {
     UnifiedArmSubsystem.KeyArmPosition targetPosition;
+    UnifiedArmSubsystem.RobotFacing targetFacing;
     private final UnifiedArmSubsystem arms;
 
     @Inject
@@ -17,15 +16,16 @@ public class SetArmsToPositionCommand extends BaseSetpointCommand {
         this.arms = arms;
     }
 
-    public void setTargetPosition(UnifiedArmSubsystem.KeyArmPosition keyArmPosition){
+    public void setTargetPosition(UnifiedArmSubsystem.KeyArmPosition keyArmPosition, UnifiedArmSubsystem.RobotFacing facing){
         this.targetPosition = keyArmPosition;
+        this.targetFacing = facing;
         this.setName("SetArmsTo" + keyArmPosition +"PositionCommand");
     }
     @Override
     public void initialize() {
         log.info("Initializing with target position");
         if(targetPosition != null){
-            arms.setTargetValue(arms.getKeyArmPosition(targetPosition));
+            arms.setTargetValue(arms.getKeyArmCoordinates(targetPosition, targetFacing));
         }
     }
     @Override
