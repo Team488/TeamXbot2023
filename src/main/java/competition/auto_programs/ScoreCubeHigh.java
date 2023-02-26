19c836039666f8ff62f7812ccec8166242bd402f
JohnGilb <johngilb@gmail.com>
2023-02-25 16:18:38 -0800

    Make ScoreCubeHigh more flexible to alliance location

--- src/main/java/competition/auto_programs/ScoreCubeHigh.java
+++ src/main/java/competition/auto_programs/ScoreCubeHigh.java
@@ -2,11 +2,14 @@
 package competition.auto_programs;
 
 import competition.subsystems.arm.UnifiedArmSubsystem;
-import competition.subsystems.arm.commands.SetArmsToPositionCommand;
+import competition.subsystems.arm.commands.SimpleSafeArmRouterCommand;
 import competition.subsystems.claw.CloseClawCommand;
 import competition.subsystems.claw.OpenClawCommand;
 import competition.subsystems.drive.DriveSubsystem;
 import competition.subsystems.drive.commands.SwerveToPointCommand;
+import competition.subsystems.pose.PoseSubsystem;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
@@ -20,12 +23,18 @@ public class ScoreCubeHigh extends SequentialCommandGroup {
     @Inject
     ScoreCubeHigh(Provider<OpenClawCommand> openClawProvider,
                   Provider<CloseClawCommand> closeClawProvider,
-                  Provider<SetArmsToPositionCommand> setArmPosProvider,
+                  Provider<SimpleSafeArmRouterCommand> setArmPosProvider,
                   Provider<SwerveToPointCommand> swerveToPointProvider,
-                  DriveSubsystem drive) {
+                  UnifiedArmSubsystem arm,
+                  DriveSubsystem drive,
+                  PoseSubsystem pose) {
+
+        // Set scoring mode to Cube
+        this.addCommands(arm.createSetGamePieceModeCommand(UnifiedArmSubsystem.GamePieceMode.Cube));
+
         //move arm to high
         var moveArmToHigh = setArmPosProvider.get();
-        moveArmToHigh.setTargetPosition(UnifiedArmSubsystem.KeyArmPosition.HighGoal, UnifiedArmSubsystem.RobotFacing.Backward);
+        moveArmToHigh.setTarget(UnifiedArmSubsystem.KeyArmPosition.HighGoal, UnifiedArmSubsystem.RobotFacing.Backward);
 
         this.addCommands(moveArmToHigh);
 
@@ -40,20 +49,31 @@ public class ScoreCubeHigh extends SequentialCommandGroup {
 
         //retract arm
         var retractArm = setArmPosProvider.get();
-        retractArm.setTargetPosition(UnifiedArmSubsystem.KeyArmPosition.FullyRetracted, UnifiedArmSubsystem.RobotFacing.Backward);
+        retractArm.setTarget(UnifiedArmSubsystem.KeyArmPosition.FullyRetracted, UnifiedArmSubsystem.RobotFacing.Backward);
         this.addCommands(retractArm);
 
         //drive out of community zone
         var moveOutOfCommunity = swerveToPointProvider.get();
         moveOutOfCommunity.setFieldRelativeMotion();
         moveOutOfCommunity.setMaxPower(0.5);
-        moveOutOfCommunity.setTargetPosition(new XYPair(185, 176), 0);
+
+        // Our target has to change if we are on red vs blue
+        moveOutOfCommunity.setTargetSupplier(
+                () -> {
+                    var wpiXY = AutoLandmarks.convertBlueToRedIfNeeded(
+                            AutoLandmarks.blueNorthOfChargingStationOutsideCommunity
+                    ).getTranslation();
+                    return new XYPair(wpiXY.getX(), wpiXY.getY());
+                },
+                () -> pose.rotateAngleBasedOnAlliance(Rotation2d.fromDegrees(0)).getDegrees()
+        );
 
         var driveOut = new ParallelRaceGroup(
                 moveOutOfCommunity,
                 new WaitCommand(3));
         this.addCommands(driveOut);
+
         //stop robot
-        drive.stop();
+        this.addCommands(new InstantCommand(() -> drive.stop()));
     }
 }
