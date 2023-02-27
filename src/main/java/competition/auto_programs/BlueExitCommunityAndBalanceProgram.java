cd5229b680bde466f24c12d129b9d3d082a61b18
JohnGilb <johngilb@gmail.com>
2023-02-26 17:07:17 -0800

    Tuning up of other balance auto programs

--- src/main/java/competition/auto_programs/BlueExitCommunityAndBalanceProgram.java
+++ src/main/java/competition/auto_programs/BlueExitCommunityAndBalanceProgram.java
@@ -2,10 +2,16 @@ package competition.auto_programs;
 
 import competition.subsystems.arm.UnifiedArmSubsystem;
 import competition.subsystems.drive.commands.AutoBalanceCommand;
+import competition.subsystems.drive.commands.BrakeCommand;
 import competition.subsystems.drive.commands.SwerveSimpleTrajectoryCommand;
+import competition.subsystems.drive.commands.VelocityMaintainerCommand;
 import competition.subsystems.drive.commands.XbotSwervePoint;
+import competition.subsystems.pose.PoseSubsystem;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 
 import javax.inject.Inject;
 import javax.inject.Provider;
@@ -18,33 +24,64 @@ import java.util.List;
 public class BlueExitCommunityAndBalanceProgram extends SequentialCommandGroup {
     @Inject
     BlueExitCommunityAndBalanceProgram(Provider<AutoBalanceCommand> autoBalanceCommandProvider,
-                                       SwerveSimpleTrajectoryCommand swerveSimpleTrajectoryCommand,
-                                       AutoLandmarks landmarks,
-                                       UnifiedArmSubsystem armSubsystem){
-        InstantCommand setPitchCompensation = new InstantCommand(() -> armSubsystem.setPitchCompensation(true));
-        this.addCommands(setPitchCompensation);
-        // Swerve points are X, Y, degrees, and seconds spent going to that point.
-        XbotSwervePoint turnAroundTowardsGoal = new XbotSwervePoint
-                (landmarks.blueLowerGamePieceSideMidCheckpoint.getX(), landmarks.blueLowerGamePieceSideMidCheckpoint.getY(), -180, 0.5);
-        XbotSwervePoint moveTowardsFieldSideCheckpoint = new XbotSwervePoint
-                (landmarks.blueToUpperAndLowerFieldCheckpoint.getX(), landmarks.blueToUpperAndLowerFieldCheckpoint.getY(), -180, 0.5);
-        XbotSwervePoint goTowardsChargeStation = new XbotSwervePoint
-                (landmarks.blueChargeStationCenter.getX(), landmarks.blueChargeStationCenter.getY(), -180, 1.0);
+                                       SwerveSimpleTrajectoryCommand mantleChargePlate,
+                                       AutoLandmarks landmarks, VelocityMaintainerCommand velocityMaintainer,
+                                       PoseSubsystem pose, BrakeCommand brake) {
 
-        swerveSimpleTrajectoryCommand.setKeyPoints(
-                new ArrayList<>(List.of(
-                        turnAroundTowardsGoal,
-                        moveTowardsFieldSideCheckpoint,
-                        goTowardsChargeStation
-                )));
-        var autoBalance = autoBalanceCommandProvider.get();
+        // Force set our current position - just about to exit the community
+        var forcePosition = new InstantCommand(
+                () -> {
+                    var translation =
+                            AutoLandmarks.convertBlueToRedIfNeeded(AutoLandmarks.blueBelowChargeStation)
+                                    .getTranslation();
+                    var rotation = pose.rotateAngleBasedOnAlliance(Rotation2d.fromDegrees(-180));
+                }
+        );
+        this.addCommands(forcePosition);
 
-        this.addCommands(swerveSimpleTrajectoryCommand);
-        this.addCommands(autoBalance);
+        // TODO: Increase power at competition.
+        mantleChargePlate.setMaxPower(0.5);
+        mantleChargePlate.setKeyPointsProvider(
+                () -> {
+                    var blueLowerGamePieceSideMidCheckpoint =
+                            AutoLandmarks.convertBlueToRedIfNeeded(landmarks.blueLowerGamePieceSideMidCheckpoint);
+                    var blueToUpperAndLowerFieldCheckpoint =
+                            AutoLandmarks.convertBlueToRedIfNeeded(landmarks.blueToUpperAndLowerFieldCheckpoint);
+                    var blueChargeStationMantleFromRight =
+                            AutoLandmarks.convertBlueToRedIfNeeded(landmarks.blueChargeStationMantleFromRight);
 
-        InstantCommand resetPitchCompensation = new InstantCommand(() -> armSubsystem.setPitchCompensation(false));
-        this.addCommands(resetPitchCompensation);
+                    XbotSwervePoint blueLowerGamePieceSideMidCheckpointPoint = new XbotSwervePoint
+                            (blueLowerGamePieceSideMidCheckpoint.getX(),
+                                    blueLowerGamePieceSideMidCheckpoint.getY(),
+                                    -180,
+                                    1.0);
+                    XbotSwervePoint blueToUpperAndLowerFieldCheckpointPoint = new XbotSwervePoint
+                            (blueToUpperAndLowerFieldCheckpoint.getX(),
+                                    blueToUpperAndLowerFieldCheckpoint.getY(),
+                                    -180,
+                                    1.0);
+                    XbotSwervePoint blueChargeStationMantleFromRightPoint = new XbotSwervePoint
+                            (blueChargeStationMantleFromRight.getX(),
+                                    blueChargeStationMantleFromRight.getY(),
+                                    -180,
+                                    0.5);
 
+                    return new ArrayList<>(List.of(blueLowerGamePieceSideMidCheckpointPoint,
+                            blueToUpperAndLowerFieldCheckpointPoint,
+                            blueChargeStationMantleFromRightPoint));
+                }
+        );
+
+        var mantleAndBalance = new SequentialCommandGroup(
+                mantleChargePlate.deadlineWith(new WaitCommand(5.0)), // Mantle but give up after 5 sec.
+                new ParallelCommandGroup(
+                        autoBalanceCommandProvider.get(),
+                        velocityMaintainer
+                )
+        ).deadlineWith(new WaitCommand(14.5));
+
+        this.addCommands(mantleAndBalance);
+        this.addCommands(brake);
 
     }
 }
