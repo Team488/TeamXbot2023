558a2192eb44a7d5798df3631950f50292d6bc33
John Gilbert <johngilb@gmail.com>
2023-03-03 14:42:29 -0800

    Some basic low scoring autos using the collector

--- src/main/java/competition/auto_programs/AutoLandmarks.java
+++ src/main/java/competition/auto_programs/AutoLandmarks.java
@@ -23,10 +23,11 @@ public class AutoLandmarks {
     public static Pose2d blueUpperGamePieceSideMidCheckpoint = new Pose2d(220,195, Rotation2d.fromDegrees(-180));
 
     // Charge station checkpoints + charge station coordinate
-    public static Pose2d blueChargeStationCenter = new Pose2d(150,110, Rotation2d.fromDegrees(-180));
+    public static Pose2d blueChargeStationCenter = new Pose2d(157,110, Rotation2d.fromDegrees(-180));
     public static Pose2d blueChargeStationMantleFromLeft = new Pose2d(140,110, Rotation2d.fromDegrees(-180));
     public static Pose2d blueChargeStationMantleFromRight = new Pose2d(170,110, Rotation2d.fromDegrees(-180));
     public static Pose2d blueBelowChargeStation = new Pose2d(170,30, Rotation2d.fromDegrees(-180));
+    public static Pose2d blueEastOfChargeStation = new Pose2d(235, 110, Rotation2d.fromDegrees(-180));
 
     public static Pose2d blueToUpperAndLowerCommunityCheckpoint = new Pose2d(87,110, Rotation2d.fromDegrees(-180));
     public static Pose2d blueToUpperAndLowerFieldCheckpoint = new Pose2d(239,110, Rotation2d.fromDegrees(-180));
