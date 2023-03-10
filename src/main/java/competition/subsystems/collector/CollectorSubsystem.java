024d9871b55e5b562d7b424820166dffb3003bbc
Stephanie <sw7859305@gmail.com>
2023-03-09 20:02:21 -0800

    checks if game piece is collected

--- src/main/java/competition/subsystems/collector/CollectorSubsystem.java
+++ src/main/java/competition/subsystems/collector/CollectorSubsystem.java
@@ -7,6 +7,7 @@ import xbot.common.command.BaseSubsystem;
 import xbot.common.controls.actuators.XCANSparkMax;
 import xbot.common.controls.actuators.XSolenoid;
 import xbot.common.controls.sensors.XAnalogInput;
+import xbot.common.controls.sensors.XTimer;
 import xbot.common.properties.DoubleProperty;
 import xbot.common.properties.PropertyFactory;
 
@@ -21,11 +22,16 @@ public class CollectorSubsystem extends BaseSubsystem {
     public DoubleProperty ejectPower;
     private CollectorState currentState;
     final ElectricalContract contract;
+    public DoubleProperty currentMotorVelocity;
+    public double intakeTime;
+    public double currentIntakeTime;
+    boolean gamePieceCollected = false;
+    boolean intake = false;
     private int loopCount;
 
     private XAnalogInput pressureSensor;
 
-    public enum CollectorState{
+    public enum CollectorState {
         Extended,
         Retracted
     }
@@ -33,58 +39,66 @@ public class CollectorSubsystem extends BaseSubsystem {
     @Inject
     public CollectorSubsystem(XCANSparkMax.XCANSparkMaxFactory sparkMaxFactory, PropertyFactory pf,
                               XSolenoid.XSolenoidFactory xSolenoidFactory, XAnalogInput.XAnalogInputFactory analogInputFactory,
-                              ElectricalContract eContract){
+                              ElectricalContract eContract) {
         this.contract = eContract;
         this.currentState = CollectorState.Retracted;
-        if(contract.isCollectorReady()){
-            this.collectorMotor = sparkMaxFactory.create(eContract.getCollectorMotor(),getPrefix(),"CollectorMotor");
+        if (contract.isCollectorReady()) {
+            this.collectorMotor = sparkMaxFactory.create(eContract.getCollectorMotor(), getPrefix(), "CollectorMotor");
             this.collectorSolenoid = xSolenoidFactory.create(eContract.getCollectorSolenoid().channel);
             collectorMotor.setSmartCurrentLimit(5);
             pressureSensor = analogInputFactory.create(eContract.getPressureSensor().channel);
         }
         pf.setPrefix(this);
-        intakePower = pf.createPersistentProperty("intakePower",1);
+        intakePower = pf.createPersistentProperty("intakePower", 1);
         ejectPower = pf.createPersistentProperty("retractPower", -1);
-
 
 
     }
 
-    private void changeCollector(CollectorState state){
+    private void changeCollector(CollectorState state) {
         currentState = state;
-        if(state == CollectorState.Extended){
+        if (state == CollectorState.Extended) {
             collectorSolenoid.setOn(true);
         } else if (state == CollectorState.Retracted) {
             collectorSolenoid.setOn(false);
         }
     }
-    public CollectorState getCollectorState(){
+
+    public CollectorState getCollectorState() {
         return currentState;
     }
 
-    public void extend(){
+    public void extend() {
         changeCollector(CollectorState.Extended);
     }
 
-    public void retract(){
+    public void retract() {
         changeCollector(CollectorState.Retracted);
     }
 
-    private void setMotorPower(double power){
-        if(contract.isCollectorReady()){
+    private void setMotorPower(double power) {
+        if (contract.isCollectorReady()) {
             collectorMotor.set(power);
         }
     }
 
-    public void intake(){
+    public void intake() {
         setMotorPower(intakePower.get());
-    }
-    public void eject(){
-        setMotorPower(ejectPower.get());
+        if (!intake) {
+            intakeTime = XTimer.getFPGATimestamp();
+        }
+        currentIntakeTime = XTimer.getFPGATimestamp();
+        intake = true;
     }
 
-    public void stop(){
+    public void eject() {
+        setMotorPower(ejectPower.get());
+        intake = false;
+    }
+
+    public void stop() {
         setMotorPower(0);
+        intake = false;
     }
 
     public Command getCollectThenRetractCommand() {
@@ -120,6 +134,16 @@ public class CollectorSubsystem extends BaseSubsystem {
             if (loopCount % 250 == 0) {
                 log.info("PressureSensorValue:" + pressureSensor.getVoltage());
             }
+
+            if (currentIntakeTime - intakeTime > 0.5) {
+                //check current RPM is less than 0.5
+                currentMotorVelocity.set(collectorMotor.getVelocity());
+                gamePieceCollected = currentMotorVelocity.get() < 0.5;
+            }
         }
+    }
+
+    public boolean getGamePieceCollected() {
+        return gamePieceCollected;
     }
 }
