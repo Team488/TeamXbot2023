fc49f4a924858e8a67649bb2b88fbb18accf2776
SHORT-LIFE-CHOSE-ME <118636986+SHORT-LIFE-CHOSE-ME@users.noreply.github.com>
2023-03-11 11:40:24 -0800

    deleted unnecessary pose

--- src/main/java/competition/subsystems/drive/commands/MoveLeftInchByInchCommand.java
+++ src/main/java/competition/subsystems/drive/commands/MoveLeftInchByInchCommand.java
@@ -3,13 +3,12 @@ package competition.subsystems.drive.commands;
 import javax.inject.Inject;
 import javax.inject.Provider;
 
-import competition.subsystems.pose.PoseSubsystem;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import xbot.common.math.XYPair;
 
 public class MoveLeftInchByInchCommand extends SequentialCommandGroup {
 	@Inject
-    MoveLeftInchByInchCommand(Provider<SwerveToPointCommand> swerveToPointCommandProvider, PoseSubsystem pose){
+    MoveLeftInchByInchCommand(Provider<SwerveToPointCommand> swerveToPointCommandProvider){
         var moveLeft = swerveToPointCommandProvider.get();
         moveLeft.setRobotRelativeMotion();
         moveLeft.setTargetPosition(new XYPair(0,-2), 0);
--- src/main/java/competition/subsystems/drive/commands/MoveRightInchByInchCommand.java
+++ src/main/java/competition/subsystems/drive/commands/MoveRightInchByInchCommand.java
@@ -3,13 +3,12 @@ package competition.subsystems.drive.commands;
 import javax.inject.Inject;
 import javax.inject.Provider;
 
-import competition.subsystems.pose.PoseSubsystem;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import xbot.common.math.XYPair;
 
 public class MoveRightInchByInchCommand extends SequentialCommandGroup {
 	@Inject
-    MoveRightInchByInchCommand(Provider<SwerveToPointCommand> swerveToPointCommandProvider, PoseSubsystem pose){
+    MoveRightInchByInchCommand(Provider<SwerveToPointCommand> swerveToPointCommandProvider){
         var moveLeft = swerveToPointCommandProvider.get();
         moveLeft.setRobotRelativeMotion();
         moveLeft.setTargetPosition(new XYPair(0,2), 0);
