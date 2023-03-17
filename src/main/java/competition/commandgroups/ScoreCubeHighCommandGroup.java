44284bb030425e2bc70416a99c6a519c5a0b0835
Stephanie <sw7859305@gmail.com>
2023-03-16 18:50:32 -0700

    fix error

--- src/main/java/competition/commandgroups/ScoreCubeHighCommandGroup.java
+++ src/main/java/competition/commandgroups/ScoreCubeHighCommandGroup.java
@@ -5,10 +5,10 @@ import competition.subsystems.arm.UnifiedArmSubsystem;
 import competition.subsystems.arm.commands.SimpleXZRouterCommand;
 import competition.subsystems.claw.ClawGripperMotorSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
-import edu.wpi.first.wpilibj2.command.*;
 
 import javax.inject.Inject;
 import javax.inject.Provider;
