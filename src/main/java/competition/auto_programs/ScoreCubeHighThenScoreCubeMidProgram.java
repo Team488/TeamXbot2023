47512893970e909fccf52bd94fdd7b6c23dc4d66
Stephanie <sw7859305@gmail.com>
2023-03-16 20:48:22 -0700

    move to game piece

--- src/main/java/competition/auto_programs/ScoreCubeHighThenScoreCubeMidProgram.java
+++ src/main/java/competition/auto_programs/ScoreCubeHighThenScoreCubeMidProgram.java
@@ -2,11 +2,17 @@ package competition.auto_programs;
 
 import competition.commandgroups.CollectionSequenceCommandGroup;
 import competition.commandgroups.ScoreCubeHighCommandGroup;
+import competition.commandgroups.ScoreCubeMidCommandGroup;
+import competition.subsystems.arm.UnifiedArmSubsystem;
+import competition.subsystems.arm.commands.SimpleXZRouterCommand;
+import competition.subsystems.claw.CloseClawCommand;
 import competition.subsystems.drive.DriveSubsystem;
 import competition.subsystems.drive.commands.SwerveToPointCommand;
 import competition.subsystems.pose.PoseSubsystem;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import xbot.common.math.XYPair;
 
 import javax.inject.Inject;
@@ -16,7 +22,12 @@ public class ScoreCubeHighThenScoreCubeMidProgram extends SequentialCommandGroup
     ScoreCubeHighThenScoreCubeMidProgram(ScoreCubeHighThenLeaveProgram scoreCubeHigh,
                                          CollectionSequenceCommandGroup collect,
                                          SwerveToPointCommand moveOutCommunity,
+                                         SwerveToPointCommand turnToCube,
+                                         SwerveToPointCommand driveToScoreMid,
+                                         ScoreCubeMidCommandGroup scoreCubeMid,
+                                         SimpleXZRouterCommand retractArm,
                                          DriveSubsystem drive,
+                                         CloseClawCommand closeClaw,
                                          PoseSubsystem pose) {
         this.addCommands(scoreCubeHigh);
 
@@ -29,10 +40,29 @@ public class ScoreCubeHighThenScoreCubeMidProgram extends SequentialCommandGroup
             return new XYPair(XY.getX(), XY.getY());
         }, () -> pose.rotateAngleBasedOnAlliance(Rotation2d.fromDegrees(-180)).getDegrees());
 
-        //collect game piece
-        this.addCommands(collect);
+        var driveToGamePieceAndCollect = new ParallelCommandGroup(moveOutCommunity, collect);
+        this.addCommands(driveToGamePieceAndCollect);
 
-        //score cube mid
+        //drive to mid scoring position
+        driveToScoreMid.setFieldRelativeMotion();
+        driveToScoreMid.setMaxPower(0.5);
+        driveToScoreMid.setMaxTurningPower(0.5);
+
+      /*  driveToScoreMid.setTargetSupplier(() -> {
+            var XY = AutoLandmarks.convertBlueToRedIfNeeded(AutoLandmarks.).getTranslation();
+            return new XYPair(XY.getX(), XY.getY());
+        }, () -> pose.rotateAngleBasedOnAlliance(Rotation2d.fromDegrees(-180)).getDegrees());*/
+
+        retractArm.setKeyPointFromKeyArmPosition(UnifiedArmSubsystem.KeyArmPosition.PrepareToAcquireFromCollector,
+                UnifiedArmSubsystem.RobotFacing.Forward);
+
+        //score cube mid then retract arm
+        var retractArmAndCloseClaw = new SequentialCommandGroup(
+                new WaitCommand(1),
+                new ParallelCommandGroup(retractArm, closeClaw).deadlineWith(new WaitCommand(5))
+        );
+
+
 
 
     }
