8f582b38f42891d4fbf84f9c149f86438aeb4e4c
Stephanie <sw7859305@gmail.com>
2023-03-16 20:32:22 -0700

    moves to game piece

--- src/main/java/competition/auto_programs/ScoreCubeHighThenScoreCubeMidProgram.java
+++ src/main/java/competition/auto_programs/ScoreCubeHighThenScoreCubeMidProgram.java
@@ -5,7 +5,9 @@ import competition.commandgroups.ScoreCubeHighCommandGroup;
 import competition.subsystems.drive.DriveSubsystem;
 import competition.subsystems.drive.commands.SwerveToPointCommand;
 import competition.subsystems.pose.PoseSubsystem;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import xbot.common.math.XYPair;
 
 import javax.inject.Inject;
 
@@ -15,11 +17,22 @@ public class ScoreCubeHighThenScoreCubeMidProgram extends SequentialCommandGroup
                                          CollectionSequenceCommandGroup collect,
                                          SwerveToPointCommand moveOutCommunity,
                                          DriveSubsystem drive,
-                                         PoseSubsystem pose){
+                                         PoseSubsystem pose) {
         this.addCommands(scoreCubeHigh);
-        //rotate and collect
 
+        moveOutCommunity.setFieldRelativeMotion();
+        moveOutCommunity.setMaxPower(0.5);
+        moveOutCommunity.setMaxTurningPower(0.5);
+        //move to game piece
+        moveOutCommunity.setTargetSupplier(() -> {
+            var XY = AutoLandmarks.convertBlueToRedIfNeeded(AutoLandmarks.blueNorthOfChargingStationGamePiece).getTranslation();
+            return new XYPair(XY.getX(), XY.getY());
+        }, () -> pose.rotateAngleBasedOnAlliance(Rotation2d.fromDegrees(-180)).getDegrees());
 
+        //collect game piece
+        this.addCommands(collect);
+
+        //score cube mid
 
 
     }
