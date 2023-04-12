1de4563286f5af28019daefa282a2826f15536eb
JohnGilb <johngilb@gmail.com>
2023-04-11 21:08:01 -0700

    Cubes have more X/Z permissibility than cones

--- src/test/java/competition/subsystems/arm/UnifiedArmMaintainerTest.java
+++ src/test/java/competition/subsystems/arm/UnifiedArmMaintainerTest.java
@@ -31,7 +31,6 @@ public class UnifiedArmMaintainerTest extends BaseCompetitionTest {
     }
 
     @Test
-    @Ignore("TODO: Investigate")
     public void testBrakeChangeState() {
         assertTrue(arms.areBrakesEngaged());
         assertEquals(1, arms.getCurrentValue().x, 0.001);
@@ -42,8 +41,9 @@ public class UnifiedArmMaintainerTest extends BaseCompetitionTest {
         // Since we are very far from our target, the brake should disengage.
         assertFalse(arms.areBrakesEngaged());
 
+        setArmAngles(45, -90);
         // Now, let's move to a position where the brakes should engage.
-        arms.setTargetValue(new XYPair(1+maintainer.getLowerArmErrorThresholdToEngageBrake() - 0.1, -90));
+        arms.setTargetValue(new XYPair(45+maintainer.getLowerArmErrorThresholdToEngageBrake() - 0.1, -90));
         maintainer.execute();
 
         // Since we are very close to our target, but this is just the first time we've been close, the brakes should not engage.
@@ -57,19 +57,55 @@ public class UnifiedArmMaintainerTest extends BaseCompetitionTest {
         assertTrue(arms.areBrakesEngaged());
 
         // To test hysterisis, move the position just outside the threshold.
-        arms.setTargetValue(new XYPair(1+maintainer.getLowerArmErrorThresholdToDisengageBrake() - 0.1, -90));
+        arms.setTargetValue(new XYPair(45+maintainer.getLowerArmErrorThresholdToDisengageBrake() - 0.1, -90));
         maintainer.execute();
 
         // Even though we are outside our brake engage threshold, we haven't moved outside the disengage threshold.
         assertTrue(arms.areBrakesEngaged());
 
         // Now, move outside the disengage threshold.
-        arms.setTargetValue(new XYPair(1+maintainer.getLowerArmErrorThresholdToDisengageBrake() + 0.1, -90));
+        arms.setTargetValue(new XYPair(45+maintainer.getLowerArmErrorThresholdToDisengageBrake() + 0.1, -90));
         maintainer.execute();
 
         // Now that we are outside the disengage threshold, the brakes should disengage.
         assertFalse(arms.areBrakesEngaged());
     }
+
+    @Test
+    public void testCubeModeExpandedErrorThreshold() {
+        setArmAngles(45, 90);
+        arms.setGamePieceMode(UnifiedArmSubsystem.GamePieceMode.Cone);
+        arms.setTargetValue(new XYPair(45, 90));
+        maintainer.execute();
+        timer.advanceTimeInSecondsBy(2);
+        maintainer.execute();
+        assertTrue(arms.isMaintainerAtGoal());
+
+        // disengage the brake
+        arms.setTargetValue(new XYPair(90, 90));
+        maintainer.execute();
+        assertFalse(arms.areBrakesEngaged());
+        // now actually test the situation
+
+        arms.setTargetValue(new XYPair(45, 90));
+        setArmAngles(42, 92);
+        maintainer.execute();
+        timer.advanceTimeInSecondsBy(0.25);
+        maintainer.execute();
+        assertFalse(arms.isMaintainerAtGoal());
+
+        arms.setGamePieceMode(UnifiedArmSubsystem.GamePieceMode.Cube);
+        maintainer.execute();
+        timer.advanceTimeInSecondsBy(0.45);
+        maintainer.execute();
+        assertTrue(arms.isMaintainerAtGoal());
+
+        var xypos = arms.getCurrentXZCoordinates();
+
+
+    }
+
+
 
     private void setArmAngles(double lowerArmAngle, double upperArmAngle) {
         ((MockDutyCycleEncoder)arms.lowerArm.absoluteEncoder).setRawPosition(lowerArmAngle/360.0);
